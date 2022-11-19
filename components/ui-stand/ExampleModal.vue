50dc2d54ae26d4e671588a12b6d0e88e2ec85607
a.drozdov <a.drozdov@b2b-center.ru>
2022-11-19 11:51:55 +0530

    modal and sidebar

---
+++ components/ui-stand/ExampleModal.vue
@@ -0,0 +1,49 @@
+<template>
+  <abstract-ui-example>
+    <template #header>
+      Modal
+    </template>
+    <template #component>
+      <modal v-model="isShowModal">
+        <template #title>
+          <h3 class="text-2xl font-semibold text-gray-900 dark:text-white">
+            Terms of Service
+          </h3>
+        </template>
+        <template #body>
+          <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+          </p>
+        </template>
+        <template #footer>
+          <ui-button appearance="secondary">
+            Reschedule
+          </ui-button>
+          <ui-button appearance="secondary">
+            Mark and dismiss
+          </ui-button>
+          <ui-button class="ml-5">
+            Add info
+          </ui-button>
+        </template>
+      </modal>
+    </template>
+    <template #settings>
+      <ui-button :appearance="isShowModal ? 'secondary' : 'primary'" @click="isShowModal = true">
+        Open Modal
+      </ui-button>
+    </template>
+  </abstract-ui-example>
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'nuxt-property-decorator';
+import AbstractUiExample from './AbstractUiExample.vue';
+import Modal from '~/components/ui/Modal.vue';
+import UIButton from '~/components/ui/Button.vue';
+
+@Component({ components: { Modal, AbstractUiExample, UIButton } })
+export default class ExampleModal extends Vue {
+  isShowModal = false;
+}
+</script>
---
+++ components/ui-stand/ExampleSidebar.vue
@@ -0,0 +1,39 @@
+<template>
+  <abstract-ui-example>
+    <template #header>
+      Sidebar
+    </template>
+    <template #component>
+      <sidebar v-model="isShowSidebar">
+        <template #content>
+          <p class="mb-6 text-sm text-gray-500 dark:text-gray-400">
+            Supercharge your hiring by taking advantage of our <a href="#" class="text-blue-600 underline dark:text-blue-500 hover:no-underline">limited-time sale</a> for Flowbite Docs + Job Board. Unlimited access to over 190K top-ranked candidates and the #1 design job board.
+          </p>
+        </template>
+        <template #footer>
+          <ui-button appearance="secondary" class="w-full">
+            Reschedule
+          </ui-button>
+        </template>
+      </sidebar>
+    </template>
+    <template #settings>
+      <ui-button :appearance="isShowSidebar ? 'secondary' : 'primary'" @click="isShowSidebar = true">
+        Open sidebar
+      </ui-button>
+    </template>
+  </abstract-ui-example>
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'nuxt-property-decorator';
+import AbstractUiExample from './AbstractUiExample.vue';
+import Modal from '~/components/ui/Modal.vue';
+import UIButton from '~/components/ui/Button.vue';
+import Sidebar from '~/components/ui/SIdebar.vue';
+
+@Component({ components: { Sidebar, Modal, AbstractUiExample, UIButton } })
+export default class ExampleSidebar extends Vue {
+  isShowSidebar = false;
+}
+</script>
